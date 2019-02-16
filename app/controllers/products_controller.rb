49647aa7846e8db968485f9c6e48f1bfc283ac3d
dowpm <mcdowpm@gmail.com>
2019-02-15 21:51:23 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # render json: @products
   end
 
   def inventory
@@ -26,8 +27,9 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+      format.json { render json: @product}
     end
+    # render json: @product
   end
 
   private
---
+++ app/serializers/custom_product_serializer.rb
@@ -0,0 +1,3 @@
+class CustomProductSerializer < ActiveModel::Serializer
+  attributes :name
+end
